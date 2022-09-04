092782a2339768ae94e97d855b60e44b27f60be2
KaliemSB <mycael590@gmail.com>
2022-09-04 18:44:52 -0300

    port for vercel

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const io = new Server(server);
 
 let messages = []
 
-app.get('/', (req, res) => {
+app.get('/api/chat', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -30,6 +30,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`port: ${port}`);
 });
