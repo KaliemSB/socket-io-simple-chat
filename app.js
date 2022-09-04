94e77ad3cdf3c82beba0dca508cae546470f1e92
KaliemSB <mycael590@gmail.com>
2022-09-04 18:36:33 -0300

    final project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let messages = []
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    messages.push(msg)
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.join(socket.id);
+
+  io.to(socket.id).emit("get messages", messages)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
